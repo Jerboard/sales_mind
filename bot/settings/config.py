c1fc4b69e0135f6234168a07c58656111c3739d4
dgush <dgushch@gmail.com>
2025-06-17 21:04:43 +0400

    v2.2.3

--- bot/settings/config.py
+++ bot/settings/config.py
@@ -11,7 +11,7 @@ class Config:
         pay_key = os.getenv("PAY_KEY_TEST")
         wallet_id = os.getenv("WALLET_ID_TEST")
     else:
-        token = os.getenv("TOKEN")
+        token = os.getenv("TOKEN_TEST")
         pay_key = os.getenv("PAY_KEY_TEST")
         wallet_id = os.getenv("WALLET_ID_TEST")
 
